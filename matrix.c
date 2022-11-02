3ac46548ad7d1a2c578c6771769cb9e31239aefb
MypkaXD <85798586+MypkaXD@users.noreply.github.com>
2022-11-02 20:10:47 +0300

    fix again XDDD

--- matrix.c
+++ matrix.c
@@ -8,37 +8,53 @@ int main() {
 	system("chcp 1251");
 	system("cls");
 
-	printf("Введите кол-во строк-N матрицы-A (N<=20): ");
+	int f = 20;// максимальный размер матрицы
+
 	int N;
-	scanf_s("%d", &N);
-	if (N > 20 || N <= 0){
-		printf("ERROR: Вы ввели неправильное значение N\n");
-		exit(0);
+
+	int temp = 1;
+	printf("Введите кол-во строк-N матрицы-A: ");
+	while (temp)
+	{
+		scanf_s("%d", &N);
+		if (N > f || N <= 0) {
+			printf("ERROR: Вы ввели неправильное значение N \n");
+			return 0;
+		}
+		else
+			temp = 0;
 	}
 
-	printf("Введите кол-во столбцов-M матрицы-A (M<=20): ");
 	int M;
-	scanf_s("%d", &M);
-	if (M > 20 || M <= 0) {
-		printf("ERROR: Вы ввели неправильное значение M\n");
-		exit(0);
+	temp = 1;
+	printf("Введите кол-во столбцов-M матрицы-A: ");
+	while (temp)
+	{
+		scanf_s("%d", &M);
+		if (M > f || M <= 0) {
+			printf("ERROR: Вы ввели неправильное значение M \n");
+			return 0;
+		}
+		else
+			temp = 0;
 	}
 
 	int** matrixA;
+
 	matrixA = (int**)malloc(N * sizeof(int*));
 	for (int count = 0; count < N; count++) {
 		matrixA[count] = (int*)malloc(M * sizeof(int));
 	}
 
-	for (int count = 0; count < N; count++){
-		for (int COUNT = 0; COUNT < M; COUNT++){
+	for (int count = 0; count < N; count++) {
+		for (int COUNT = 0; COUNT < M; COUNT++) {
 			printf("a[%d][%d] = ", count, COUNT);
 			scanf_s("%d", &matrixA[count][COUNT]);
 		}
 	}
-
-	for (int count = 0; count < N; count++){
-		for (int COUNT = 0; COUNT < M; COUNT++){
+	printf("Матрица А:\n");
+	for (int count = 0; count < N; count++) {
+		for (int COUNT = 0; COUNT < M; COUNT++) {
 			printf("%5d ", matrixA[count][COUNT]);
 		}
 		printf("\n");
@@ -46,40 +62,53 @@ int main() {
 
 	printf("================================================\n");
 
-	int *checker;
+	int* checker;
+
 	checker = (int*)malloc(N * sizeof(int));
 	for (int count = 0; count < N; count++) {
-		for (int COUNT = 0; COUNT < M-1; COUNT++) {
-			if ((matrixA[count][COUNT] > matrixA[count][COUNT + 1]) && (matrixA[count][COUNT] % 2 != 0))
-				checker[count] = count;
-			else {
-				checker[count] = -1;
+		if (M > 1) {
+			for (int COUNT = 0; COUNT < M - 1; COUNT++) {
+				if ((matrixA[count][COUNT] > matrixA[count][COUNT + 1]) && (matrixA[count][COUNT] % 2 != 0))
+					checker[count] = count;
+				else {
+					checker[count] = -1;
+				}
+			}
+		}
+		else if (M == 1) {
+			for (int COUNT = 0; COUNT < M; COUNT++) {
+				if (matrixA[count][COUNT] % 2 != 0)
+					checker[count] = count;
+				else {
+					checker[count] = -1;
+				}
 			}
 		}
 	}
 
-	int N2;
-	
-	int maxIndex = -1;
 
 	for (int count = 0; count < N; count++) {
 		printf("%d\t", checker[count]);
 	}
 
+	int maxIndex = -1;
+
 	for (int count = 0; count < N; count++) {
 		if (checker[count] >= maxIndex) {
 			maxIndex = checker[count];
 		}
-		else 
+		else
 			maxIndex = maxIndex;
 	}
 	printf("Инедкс строки = ");
 	printf("%d", maxIndex);
 	printf("\n================================================\n");
 
+	int N2;
+
 	if (maxIndex >= 0) {
 		N2 = N - 1;
-		if (N2 <= 0) {
+		if (N2 < 0) {
 			printf("ERROR: неправильная длина матрицы");
 			exit(0);
 		}
@@ -94,7 +123,7 @@ int main() {
 	for (int count = 0; count < N2; count++) {
 		matrixB[count] = (int*)malloc(M * sizeof(int));
 	}
-	
+
 	for (int count = 0; count < N; count++) {//копируем элементы matrixA в матрицу B
 		for (int COUNT = 0; COUNT < M; COUNT++) {
 			if (maxIndex == -1) {
@@ -108,14 +137,14 @@ int main() {
 					matrixB[count][COUNT] = matrixA[count][COUNT];
 				}
 				else if (count > maxIndex) {
-					matrixB[count-1][COUNT] = matrixA[count][COUNT];
+					matrixB[count - 1][COUNT] = matrixA[count][COUNT];
 				}
 			}
 		}
 	}
 
-	
-	printf("\n================================================\n");
+
+	printf("Матрица B: \n");
 
 	for (int count = 0; count < N2; count++) {//печатаем matrixB
 		for (int COUNT = 0; COUNT < M; COUNT++) {
@@ -123,7 +152,7 @@ int main() {
 		}
 		printf("\n");
 	}
-	
+
 
 	for (int count = 0; count < N2; count++)//удаление массива matrixB
 		free(matrixB[count]);
